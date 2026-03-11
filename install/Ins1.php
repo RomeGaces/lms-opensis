96a1a2f729d2a7456d42a4294a4b50dcf4317d94
Jerome Gaces <jerome.gaces@zywave.com>
2026-03-11 15:10:36 -0500

    update on install file

--- install/Ins1.php
+++ install/Ins1.php
@@ -26,7 +26,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #***************************************************************************************
-error_reporting(E_ERROR)
+error_reporting(E_ERROR);
 include '../functions/ParamLibFnc.php';
 require_once("../functions/PragRepFnc.php");
 session_start();
